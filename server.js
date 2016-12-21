5bb655eb840c5b5247bf8519b7a2b38b65f53fb6
Christopher Brown <Chrisbrown0419@gmail.com>
2016-12-21 10:20:11 -0700

    Server built by charlie

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(bodyParser.json(), bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost/ExpressMVC', (error)=>{
+    if(error) {
+        console.log("Error connecting to database!");
+    } else {
+        console.log("Connected to database!");
+    }
+});
+
+app.get('/', (req, res)=>{
+        res.sendFile('webpage.txt', {root:"./public"} );
+});
+
+app.listen(PORT, (err)=>{
+    if(err) {
+        console.log("Server error: ", err);
+    } else {
+        console.log("Server up and running on port: ", PORT);
+    }
+});
